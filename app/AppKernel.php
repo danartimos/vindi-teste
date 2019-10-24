c0557dc8abc2be118c660c1b1f8a0af424b6f2c4
Dan Artimos <artimos.dan@gmail.com>
2019-10-24 14:40:09 -0300

    Update AppKernel.php
    sesio

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,12 +19,12 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'test', 'prod'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 
-            if ('dev' === $this->getEnvironment()) {
+            if ('dev' === $this->getEnvironment() || 'prod' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             }
